2ac639133a4acc948f7198c6a226af4c9bd30a41
Shahryar Tayeb <48182832+shtayeb@users.noreply.github.com>
2023-07-24 14:03:31 +0430

    Update news.py

--- test/news.py
+++ test/news.py
@@ -5,7 +5,7 @@ url = "https://google-news.p.rapidapi.com/v1/topic_headlines"
 querystring = {"lang":"en","country":"US","topic":"ENTERTAINMENT"}
 
 headers = {
-    'x-rapidapi-key': "ef08742670msh47b686485cc7085p1f3af6jsn8877ae9469af",
+    'x-rapidapi-key': "<API_URL>",
     'x-rapidapi-host': "google-news.p.rapidapi.com"
     }
 
