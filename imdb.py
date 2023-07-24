be31513047e4ebea4056a14f56fdffdbc55b442e
Shahryar Tayeb <48182832+shtayeb@users.noreply.github.com>
2023-07-24 13:51:17 +0430

    Update imdb.py

--- imdb.py
+++ imdb.py
@@ -3,7 +3,7 @@ class imdb():
         def search_title(self,title):
                 movies = {}
                 import json, requests
-                baseurl = 'http://omdbapi.com/?s='+title+'&plot=full&apikey=36fb159c' #only submitting the title parameter
+                baseurl = 'http://omdbapi.com/?s='+title+'&plot=full&apikey=<YOUR_KEY>' #only submitting the title parameter
                 response = requests.get(baseurl)
                 if response.status_code == 200:
                         data = response.text    
